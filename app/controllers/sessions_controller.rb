ca4ad8726e1dd6520ef5b1b55c511fc0b8e42319
rafapavez98 <repavez@miuandes.cl>
2022-09-25 20:25:42 -0300

    Student view created for teacher

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,9 @@ class SessionsController < ApplicationController
     admin_teacher = AdminTeacher.find_by(email: params[:session][:email].downcase)
     if student && student.authenticate(params[:session][:password]) 
       session[:student_id] = student.id
-      flash[:notice] = "Logged in successfully."
       redirect_to "/tasks/index"
     elsif admin_teacher && admin_teacher.authenticate(params[:session][:password])
       session[:admin_teacher_id] = admin_teacher.id
-      flash[:notice] = "Logged in successfully."
       redirect_to root_url
     else
       flash.now[:alert] = "There was something wrong with your login details."
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,15 @@
 class Student < ApplicationRecord
     has_secure_password
     has_many :tasks, dependent: :destroy
+
+    before_create :set_stats
+
+    private
+    def set_stats
+        self.extern_joins_stat = 0
+        self.forces_moments_stat = 0
+        self.con_rod_stat = 0
+        self.ball_joint_stat = 0
+    end
 end
+
