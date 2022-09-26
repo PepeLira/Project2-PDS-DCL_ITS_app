982ba71e54b32bb6c0203597dc428c8a995771ff
PepeLira <jflira@miuandes.cl>
2022-09-26 09:03:48 -0300

    type fix

--- db/migrate/20220926100845_remove_type_from_answer.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromAnswer < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :answers, :type
-  end
-end
--- db/migrate/20220926101002_add_answer_type_to_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswerTypeToAnswer < ActiveRecord::Migration[7.0]
-  def change
-    add_column :answers, :answer_type, :string
-  end
-end
